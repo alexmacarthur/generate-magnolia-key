6121036dfb6ae49c6c78f106ee06622345337fca
Alex MacArthur <alex.macarthur@daveramsey.com>
2019-12-20 11:26:23 -0600

    Successfully update public instance with new key.

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Generate Magnolia Key
 
-Use this script to generate a fresh key in your Magnolia admin and copy it to your clipboard.
+Use this script to generate a fresh key in your local Magnolia author instance and update it in your public instance.
 
 ## Install
 
-Follow these steps: 
+Clone this repository to your local machine:  
+`git@github.com:alexmacarthur/generate-magnolia-key.git`
 
-* `yarn install`
-* `yarn link`
+Install & link it with `yarn`:  
+`yarn install`  
+`yarn link`
 
-Now, you should be able to run `generate-magnolia-key`. 
+## Usage
+
+Run `generate-magnolia-key` in your terminal. After that, you'll be able to successfully publish and delete pages from your local author instance to your local public instance.
--- generate-magnolia-key.js
+++ generate-magnolia-key.js
@@ -1,63 +1,173 @@
 #!/usr/bin/env node
 
-const puppeteer = require('puppeteer');
-const cProcess = require('child_process');
+const puppeteer = require("puppeteer");
+const chalk = require("chalk");
 
-const USERNAME = 'superuser';
-const PASSWORD = 'superuser'
+const CREDS = {
+  username: "superuser",
+  password: "superuser"
+};
 
 /**
  * Given a page, log in using username & password.
- * 
- * @param {object} page 
+ *
+ * @param {object}
  * @return {Promise}
  */
-const authenticate = async (page) => {
-    await page.focus('#login-username');
-    await page.keyboard.type(USERNAME);
-    await page.focus('#login-password');
-    await page.keyboard.type(PASSWORD);
-    await page.click('#login-button');
-    await page.waitForNavigation();
-    return;
-}
+const authenticate = async page => {
+  await page.focus("#login-username");
+  await page.keyboard.type(CREDS.username);
+  await page.focus("#login-password");
+  await page.keyboard.type(CREDS.password);
+  await page.click("#login-button");
+  await page.waitForNavigation();
+  return;
+};
+
+/**
+ * Wait for a response from a certain endpoint, a certain number of times.
+ *
+ * @param {object}
+ * @return {Promise}
+ */
+const waitForSuccessfulResponse = async ({
+  page,
+  endOfUrl = "UIDL/?v-uiId=0",
+  numberOfTimes = 1,
+  timeout = 0
+} = {}) => {
+  for (let i = 0; i < new Array(numberOfTimes).length; i++) {
+    await page.waitForResponse(
+      response => {
+        return response.url().endsWith(endOfUrl) && response.status() === 200;
+      },
+      {
+        timeout
+      }
+    );
+  }
+
+  return;
+};
+
+/**
+ * Click an element and wait for a response. If expected response doesn't happen, do it again until it does.
+ *
+ * @param {object}
+ * @return {Promise}
+ */
+const clickUntilResponse = async ({
+  page,
+  selector,
+  clickCount = 1,
+  numberOfTimesToWaitForResponse = 1
+} = {}) => {
+  await page.waitForSelector(selector, { visible: true });
+
+  let node = await page.$(selector);
+
+  await node
+    .click({
+      clickCount
+    })
+    .catch(e => {});
+
+  try {
+    return await waitForSuccessfulResponse({
+      page,
+      timeout: 3000, // totes arbitrary
+      numberOfTimes: numberOfTimesToWaitForResponse
+    });
+  } catch (e) {
+    return await clickUntilResponse({
+      page,
+      selector,
+      clickCount,
+      numberOfTimesToWaitForResponse
+    });
+  }
+};
 
 /**
  * Return a new key after generating one in the Magnolia admin.
- * 
- * @param {object} page 
+ *
+ * @param {object}
  * @return {Promise}
  */
-const getFreshKey = async (page) => {
-    await page.goto('http://localhost:8080');
-    await authenticate(page);
-    await page.goto('http://localhost:8080/.magnolia/admincentral#app:publishing:;');
-    
-    // Don't love that this is arbitrary, but was having issues with Puppeteer's `.waitFor()` method working as expected. 
-    await page.waitFor(2000);
-    await page.click('button.v-nativebutton-btn-dialog');
-    await page.waitForResponse('http://localhost:8080/.magnolia/admincentral/UIDL/?v-uiId=0');
+const getFreshKey = async page => {
+  await page.goto("http://localhost:8080", { waitUntil: "networkidle0" });
+  await authenticate(page);
+  await page.goto(
+    "http://localhost:8080/.magnolia/admincentral#app:publishing:;",
+    { waitUntil: "domcontentloaded" }
+  );
 
-    return await page.evaluate(() => {
-        return document.querySelector('textarea.v-readonly').value;
-    });
-}
+  await clickUntilResponse({
+    page,
+    selector: "button.v-nativebutton-btn-dialog"
+  });
+
+  return await page.evaluate(() => {
+    return document.querySelector("textarea.v-readonly").value;
+  });
+};
+
+/**
+ * Update the public instance with a given key.
+ *
+ * @param {object} page
+ * @param {string} key
+ */
+const updatePublicInstance = async (page, key) => {
+  await page.goto("http://localhost:8888", { waitUntil: "networkidle0" });
+  await authenticate(page);
+  await page.goto(
+    "http://localhost:8888/.magnolia/admincentral#app:configuration:browser;/server/activation@publicKey:treeview:",
+    { waitUntil: "networkidle0" }
+  );
+
+  await clickUntilResponse({
+    page,
+    selector:
+      "tr.v-selected .v-table-cell-content:nth-child(2) .v-table-cell-wrapper",
+    clickCount: 2,
+    numberOfTimesToWaitForResponse: 2
+  });
+
+  await page.keyboard.press("Backspace");
+  await page.keyboard.type(key);
+  await page.keyboard.press("Enter");
+
+  return await waitForSuccessfulResponse({ page, numberOfTimes: 2 });
+};
 
 (async () => {
-    const browser = await puppeteer.launch({
-        args: ['--incognito']
-    });
-    const authorInstancePage = await browser.newPage();
-    const key = await getFreshKey(authorInstancePage);
-    cProcess.exec(`echo  ${key} | pbcopy`, 
-        function (err) {
-            if(err) {
-                console.log(error);
-            } else {
-                console.log(`Key copied: ${key}`);
-            }
+  console.log(
+    chalk.green("Updating Magnolia key! This may take a few seconds.")
+  );
 
-            process.exit();
-        }
-    );
+  const browserForAuthorInstance = await puppeteer.launch({
+    args: ["--incognito"]
+  });
+  const browserForPublicInstance = await puppeteer.launch({
+    args: ["--incognito"]
+  });
+
+  const pageForAuthorInstance = await browserForAuthorInstance.newPage();
+  const pageForPublicInstance = await browserForPublicInstance.newPage();
+
+  console.log(chalk.yellow("Generating new key in author instance..."));
+  const newKey = await getFreshKey(pageForAuthorInstance);
+
+  console.log(chalk.yellow("Setting new key in public instance..."));
+  await updatePublicInstance(pageForPublicInstance, newKey);
+
+  await browserForAuthorInstance.close();
+  await browserForPublicInstance.close();
+
+  console.log(
+    chalk.green("Key has been updated! In case want it, here it is:\n")
+  );
+  console.log(chalk.yellow(newKey));
+  process.exit();
 })();
